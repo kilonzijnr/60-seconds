1fcf86b80674ac7b3d0ee29efe982583188027ba
kilonzijnr <vkilonzi05@gmail.com>
2021-09-27 15:19:14 +0300

    created a class for comments and upvotes

--- app/models.py
+++ app/models.py
@@ -1,4 +1,7 @@
-from typing import BinaryIO
+from typing import BinaryIO, DefaultDict
+from sqlalchemy.sql.expression import false
+
+from sqlalchemy.sql.functions import user
 from app import email
 from .import db
 from sqlalchemy.sql import func
@@ -51,6 +54,67 @@ class Pitch(db.Model):
     category = db.relationship('comment', backref='pitch',lazy='dynamic') 
     upvotes = db.relationship('Upvote', backref = 'pitch', lazy = 'dynamic')
     downvotes = db.relationship('Downvote', backref = 'pitch', lazy = 'dynamic')
+
+    @classmethod
+    def get_pitches(cls, id):
+        pitches =Pitch.query.order_by(pitch_id = id).desc().all()
+        return pitches
+   
+    def save(self):
+        db.session.add(self)
+        db.session.commit()
+
+    def __repr__(self):
+        return f'Pitch {self.description}'
+
+
+class Comment(db.Model):
+    __tablename__ = 'comments'
+
+    id = db.Column(db.Integer, primary_key=True)
+    pitch_id = db.Column(db.Integer, db.ForeignKey('pitches.id'), nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
+    description = db.Column(db.Text)
+
+    def __repr__(self):
+        return f"Comment :id: {self.id} comment: {self.description}"
+
+
+class Upvote(db.Model):
+    __tablename__ = 'upvotes'
+
+    id = db.Column(db.Integer, primary_key = True)
+    upvote = db.Column(db.Integer, default=1)
+    pitch_id = db.Column(db.Integer, db.ForeignKey('pitches.id'))
+    user_id = db.Column(db.Interger,db.ForeignKey('users.id'))
+
+    def save_upvotes(self):
+        db.session.add(self)
+        db.session.commit()
+
+    def add_upvotes(cls,id):
+        upvote_pitch = Upvote(user = current_user, pitch_id=id)
+        upvote_pitch.save_upvotes()
+
+
+    @classmethod
+    def get_all_upvotes(cls, pitch_id):
+        upvotes = Upvote.query.order_by('id').all()
+        return upvotes
+
+    def __repr__(self):
+        return f'{self.user_id}:{self.pitch_id}'
+
+
+class
+
+
+
+        
+
+
+
+    
 
 
 
